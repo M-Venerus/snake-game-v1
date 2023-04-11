15fbaf843da83015c44cea52be20ea95cdb26baa
M-Venerus <mikey.motorist@outlook.com>
2023-04-11 15:49:07 -0400

    Build up Snake class and add new methods in main, called through listeners

--- snake.py
+++ snake.py
@@ -1,11 +1,15 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0),  (-40, 0)]
+MOVE_DISTANCE = 20
+# Using constants (above) instead of coding these variables right into
+# the class so that it is easier to tweak the game later just from here.
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segnments[0]
         
     def create_snake(self):    
         for position in STARTING_POSITIONS:
@@ -16,8 +20,20 @@ class Snake:
             self.segments.append(new_segment)
 
     def move(self):
-        for seg_num in range(2, 0, -1):
+        for seg_num in range(len(self.segments) -1, 0, -1):
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
